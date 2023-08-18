0775134dd3b1a0cfd35b744fdb4de75402cbaaa3
bigasare <142269753+bigasare@users.noreply.github.com>
2023-08-19 00:27:25 +0800

    Update biggsCoin.sol

--- solProofBeginner/biggsCoin.sol
+++ solProofBeginner/biggsCoin.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract biggsToken {
+
+    // public variables here
+    string public tokenName = 'biggsToken';
+    string public tokenAbbrv = 'BT';
+
+    uint public totalSupply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public balances;
+
+    // mint function
+    function mint (address _receiver, uint _amount) public {
+        balances[_receiver] += _amount;
+        totalSupply += _amount;
+    }
+
+    // burn function
+    function burn (address _burner, uint _amount) public {
+        require(balances[_burner] >= _amount, "You need to have biggs tokens if you want to destroy them" );
+
+        balances[_burner] -= _amount;
+        totalSupply -= _amount;
+    }
+
+}
